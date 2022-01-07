c0fff1801f1f6b3e95451f73c4dfdcb34d39a96f
musmanmuneer <musman.muneer@arhamsoft.com>
2022-01-07 12:50:53 +0500

    remove confilt

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -1,89 +1,137 @@
 import React, { Component } from 'react';
 import VideoPlayer from 'react-video-js-player';
 class VideoApp extends Component {
+    person = {
+        time: [
+            [
+                0.3,
+                0.3
+            ],
+            [
+                0.4782608695652174,
+                0.5217391304347826
+            ],
+            [
+                0.5217391304347826,
+                0.5652173913043478
+            ],
+            [
+                0.6956521739130435,
+                0.7391304347826086
+            ],
+            [
+                0.7391304347826086,
+                0.782608695652174
+            ],
+            [
+                0.8695652173913043,
+                0.9130434782608695
+            ],
+            [
+                1,
+                1.0434782608695652
+            ],
+            [
+                1.173913043478261,
+                1.2173913043478262
+            ],
+            [
+                1.391304347826087,
+                1.434782608695652
+            ],
+            [
+                1.4782608695652173,
+                1.5217391304347827
+            ]
+        ]
+    };
+    
     player = {}
     state = {
-        video: {
-            src: "http://img-ys011.didistatic.com/static/didiglobal/do1_pcUZZjSG7vFlMbdr8fA6#.mp4",
-            poster: "https://i.imgur.com/dG5rl3u.jpeg",
-            type: 'video/mp4'
-        }
+    video: {
+        src: "http://img-ys011.didistatic.com/static/didiglobal/do1_pcUZZjSG7vFlMbdr8fA6#.mp4",
+        poster: "https://i.imgur.com/dG5rl3u.jpeg",
+        type: 'video/mp4'
     }
-    onPlayerReady(player) {
-        console.log("Player is ready: ", player);
-        this.player = player;
-    }
-    onVideoPlay(duration) {
-        console.log("Video played at: ", duration);
-    }
+}
+onPlayerReady(player) {
+    console.log("Player is ready: ", player);
+    this.player = player;
+}
+onVideoPlay(duration) {
+    console.log("Video played at: ", duration);
+}
 
-    onVideoPause(duration) {
-        console.log("Video paused at: ", duration);
-    }
-    onVideoTimeUpdate(duration) {
-        let intervalInSeconds = Math.floor(duration);
-        let beautifySeconds = `0:${intervalInSeconds}`;
-        let durationInPercantage = (duration / 15) * 100;
-        let barElement = document.getElementById("bar_movment");
-        barElement.style.left = durationInPercantage + '%';
-        barElement.children[0].textContent = beautifySeconds;
-        console.log(intervalInSeconds);
-        console.log("Time updated: ", duration);
-    }
+onVideoPause(duration) {
+    console.log("Video paused at: ", duration);
+}
+onVideoTimeUpdate(duration) {
+    let intervalInSeconds = Math.floor(duration);
+    let beautifySeconds = `0:${intervalInSeconds}`;
+    let durationInPercantage = (duration / 15) * 100;
+    let barElement = document.getElementById("bar_movment");
+    barElement.style.left = durationInPercantage + '%';
+    barElement.children[0].textContent = beautifySeconds;
+    console.log(intervalInSeconds);
+    console.log("Time updated: ", duration);
+}
 
-    onVideoSeeking(duration) {
-        console.log("Video seeking: ", duration);
-    }
+onVideoSeeking(duration) {
+    console.log("Video seeking: ", duration);
+}
 
-    onVideoSeeked(from, to) {
-        console.log(`Video seeked from ${from} to ${to}`);
-    }
-    onVideoEnd() {
-        console.log("Video ended");
-    }
-    pauseVideo = () => {
-        this.video.pause();
-    };
+onVideoSeeked(from, to) {
+    console.log(`Video seeked from ${from} to ${to}`);
+}
+onVideoEnd() {
+    console.log("Video ended");
+}
+pauseVideo = () => {
+    this.video.pause();
+};
 
-    MoveProgressBar(value) {
-        document.getElementsByTagName("video")[0].play()
-    }
-    render() {
-        return (
-            <>
-                <div className="vedio_wraper ">
-                    <div className="card">
-                        <div className="bar_movment" id="bar_movment" onMouseDown={() => { this.MoveProgressBar() }}>
-                            <span>33</span>
-                        </div>
-                        <div class="vedio_bar" id="Don" type="range" min="0" max="100" >
-                            <span class="box_1"></span>
-                            <span class="box_2"></span>
-                            <span class="box_3"></span>
-                            <span class="box_4"></span>
-                            <span class="box_5"> </span> 
-                            </div>
-                        <div className="vedio">
-                            <VideoPlayer id="myvideo"
-                                src={this.state.video.src}
-                                controls={true}
-                                src={this.state.video.src}
-                                poster={this.state.video.poster}
-                                width="720"
-                                height="420"
-                                onReady={this.onPlayerReady.bind(this)}
-                                onPlay={this.onVideoPlay.bind(this)}
-                                onPause={this.onVideoPause.bind(this)}
-                                onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
-                                onSeeking={this.onVideoSeeking.bind(this)}
-                                onSeeked={this.onVideoSeeked.bind(this)}
-                                onEnd={this.onVideoEnd.bind(this)}>
-                            </VideoPlayer>
-                        </div>
+MoveProgressBar(value) {
+    document.getElementsByTagName("video")[0].play()
+}
+render() {
+    return (
+        <>
+            <div className="vedio_wraper ">
+                <div className="card">
+                    <div className="bar_movment" id="bar_movment" onMouseDown={() => { this.MoveProgressBar() }}>
+                        <span>0.0</span>
+                    </div>
+                    <div className="vedio_bar" id="Don" type="range" min="0" max="100" >
+                        {/* {   
+                            this.person.time.map(data=> {
+                                return (
+                                    <span className="box_1"> {data[0]} {data[1]} </span>
+                                )
+                            })
+                        } */}
+                        <span className="box_1"> </span>
+                    </div>
+                    <div className="vedio">
+                        <VideoPlayer id="myvideo"
+                            src={this.state.video.src}
+                            controls={true}
+                            src={this.state.video.src}
+                            poster={this.state.video.poster}
+                            width="720"
+                            height="420"
+                            onReady={this.onPlayerReady.bind(this)}
+                            onPlay={this.onVideoPlay.bind(this)}
+                            onPause={this.onVideoPause.bind(this)}
+                            onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
+                            onSeeking={this.onVideoSeeking.bind(this)}
+                            onSeeked={this.onVideoSeeked.bind(this)}
+                            onEnd={this.onVideoEnd.bind(this)}>
+                        </VideoPlayer>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
+}
 }
 export default VideoApp;
--- src/Layouts/Sidebar.js
+++ src/Layouts/Sidebar.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/SideBar.css';
 import { Accordion, Button } from 'react-bootstrap';
 import icon_search from "../Images/icon_search.png";
 import itme_1 from "../Images/itme_1.png";
 import { } from 'react-bootstrap-icons';
-var data = {
-    "person": 100,
-    "dining table": 2,
-    "potted plant": 3,
-    "car": 2,
-    "All": 76,
-}
-let objKeys = Object.keys(data);
-console.log(objKeys);
+
 const Sidebar = () => {
+
+    var data = {
+        "person": 100,
+        "dining table": 2,
+        "potted plant": 3,
+        "car": 2,
+        "All": 76,
+    }
+    let objKeys = Object.keys(data);
+    var frame = {
+        "person": {
+            "frames": [
+                "7",
+                "11",
+                "12",
+                "16",
+                "17",
+                "20",
+                "23",
+                "27",
+                "32",
+                "34"
+            ]
+        },
+        "dining table": {
+            "frames": [
+                "11",
+                "17"
+            ]
+        },
+        "potted plant": {
+
+            "frames": [
+                "11",
+                "17",
+                "34"
+            ]
+        },
+        "car": {
+
+            "frames":
+                [
+                    "16",
+                    "28"
+                ]
+        }
+
+    }
+
+    console.log(objKeys);
     return (
         <>
             <div className="main-body">
@@ -21,36 +63,34 @@ const Sidebar = () => {
                     <div className='part_one'>
                         <h6>Result Details:</h6>
                         <div className="search_box">
-                            <input type="text" className="form-control" name=""  placeholder="Search" />
-                            <img src={icon_search}  type="submit" className="icon_search" alt="" />
+                            <input type="text" className="form-control" name="" placeholder="Search" />
+                            <img src={icon_search} type="submit" className="icon_search" alt="" />
                         </div>
                         <div className='Accordion-wrapar'>
                             <Accordion defaultActiveKey="0" flush>
                                 {
                                     objKeys.map((d, i, a) => {
-                                        return (
-                                            <Accordion.Item eventKey={i}>
-                                    <Accordion.Header>{d.charAt(0).toUpperCase() + d.slice(1)}<span className='number'>({data[d]})</span></Accordion.Header>
-                                    <Accordion.Body>
-                                        {/* <ul>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
-                                            <li> <img src={itme_1} alt='' /></li>
+                                        if (d != "all" && d != "All" && d != "ALL") {
+                                            return (
+                                                <Accordion.Item eventKey={i}>
+                                                    <Accordion.Header>{d.charAt(0).toUpperCase() + d.slice(1)}<span className='number'>({data[d]})</span></Accordion.Header>
+                                                    {/* {
+                                                        objKeys.map((f, i, a) => {
+                                                            return ( */}
+                                                    <Accordion.Body>
+                                                        <ul>
+                                                            <li> <img src={itme_1} alt='' /></li>
+                                                        </ul>
+                                                    </Accordion.Body>
+                                                    {/* )
+                                                        }
+                                                        )} */}
 
-                                        </ul> */}
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                        )
+                                                </Accordion.Item>
+                                            )
+                                        }
                                     })
                                 }
-                                
                             </Accordion>
                         </div>
                     </div>
